dbfeeb71479ed5c67d4c073b95daaf5aeed65c56
Alejandro Salgado <alejando.salgado@benchling.com>
2023-11-24 11:41:34 -0500

    use list reader instead of string list in softmax

--- src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
+++ src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
@@ -1,14 +1,14 @@
 import io
 from typing import Any
 
-from allotropy.parsers.lines_reader import LinesReader
+from allotropy.parsers.lines_reader import CsvReader
 from allotropy.parsers.moldev_softmax_pro.softmax_pro_structure import Data
 from allotropy.parsers.vendor_parser import VendorParser
 
 
 class SoftmaxproParser(VendorParser):
     def _parse(self, contents: io.IOBase, filename: str) -> Any:  # noqa: ARG002
-        lines_reader = LinesReader(contents, encoding=None)
+        lines_reader = CsvReader(contents, encoding=None)
         data = Data.create(lines_reader)
         return self._get_model(data)
 
--- src/allotropy/parsers/moldev_softmax_pro/softmax_pro_structure.py
+++ src/allotropy/parsers/moldev_softmax_pro/softmax_pro_structure.py
@@ -47,7 +47,7 @@ from allotropy.allotrope.models.ultraviolet_absorbance_benchling_2023_09_ultravi
     MeasurementDocumentItem as MeasurementDocumentItemAbsorbance,
     Model as ModelAbsorbance,
 )
-from allotropy.parsers.lines_reader import LinesReader
+from allotropy.parsers.lines_reader import CsvReader, ListCsvReader
 from allotropy.parsers.utils.values import (
     assert_not_none,
     natural_sort_key,
@@ -102,7 +102,7 @@ class Block:
     raw_lines: list[str]
 
     @staticmethod
-    def create(lines: list[str]) -> Block:
+    def create(lines_reader: CsvReader) -> Block:
         block_cls_by_type: dict[str, type[Block]] = {
             "Group": GroupBlock,
             "Note": NoteBlock,
@@ -110,10 +110,10 @@ class Block:
         }
 
         for key, cls in block_cls_by_type.items():
-            if lines[0].startswith(key):
-                return cls.create(lines)
+            if lines_reader.lines[0].startswith(key):
+                return cls.create(lines_reader)
 
-        error = f"unrecognized block {lines[0]}"
+        error = f"unrecognized block {lines_reader.lines[0]}"
         raise AllotropeConversionError(error)
 
 
@@ -124,17 +124,17 @@ class GroupBlock(Block):
     group_data: list[str]
 
     @staticmethod
-    def create(raw_lines: list[str]) -> GroupBlock:
+    def create(lines_reader: CsvReader) -> GroupBlock:
         group_data = []
-        for i, line in enumerate(raw_lines):
+        for i, line in enumerate(lines_reader.lines):
             if line.startswith("Group Column"):
-                group_data = raw_lines[1 : i - 1]
+                group_data = lines_reader.lines[1 : i - 1]
         # TODO handle group data
 
         return GroupBlock(
             block_type="Group",
-            raw_lines=raw_lines,
-            name=raw_lines[0][len("Group: ") :],
+            raw_lines=lines_reader.lines,
+            name=lines_reader.lines[0][len("Group: ") :],
             group_data=group_data,
         )
 
@@ -143,8 +143,8 @@ class GroupBlock(Block):
 @dataclass
 class NoteBlock(Block):
     @staticmethod
-    def create(raw_lines: list[str]) -> NoteBlock:
-        return NoteBlock(block_type="Note", raw_lines=raw_lines)
+    def create(lines_reader: CsvReader) -> NoteBlock:
+        return NoteBlock(block_type="Note", raw_lines=lines_reader.lines)
 
 
 @dataclass
@@ -209,10 +209,10 @@ class PlateBlock(Block):
     cutoff_filters: Optional[list[int]]
 
     @staticmethod
-    def create(raw_lines: list[str]) -> PlateBlock:
+    def create(lines_reader: CsvReader) -> PlateBlock:
         split_lines = [
             [value_or_none(value) for value in raw_line.split("\t")]
-            for raw_line in raw_lines
+            for raw_line in lines_reader.lines
         ]
         header = split_lines[0]
         read_mode = header[5]
@@ -295,7 +295,7 @@ class PlateBlock(Block):
 
             return cls(
                 block_type="Plate",
-                raw_lines=raw_lines,
+                raw_lines=lines_reader.lines,
                 name=name,
                 export_format=export_format,
                 read_type=read_type,
@@ -841,7 +841,7 @@ class BlockList:
     blocks: list[Block]
 
     @staticmethod
-    def create(lines_reader: LinesReader) -> BlockList:
+    def create(lines_reader: CsvReader) -> BlockList:
         return BlockList(
             blocks=[
                 Block.create(block) for block in BlockList._iter_blocks(lines_reader)
@@ -849,17 +849,17 @@ class BlockList:
         )
 
     @staticmethod
-    def _get_n_blocks(lines_reader: LinesReader) -> int:
+    def _get_n_blocks(lines_reader: CsvReader) -> int:
         if search_result := re.search(BLOCKS_LINE_REGEX, lines_reader.pop() or ""):
             return int(search_result.group(1))
         error = "unrecognized start line"
         raise AllotropeConversionError(error)
 
     @staticmethod
-    def _iter_blocks(lines_reader: LinesReader) -> Iterator[list[str]]:
+    def _iter_blocks(lines_reader: CsvReader) -> Iterator[CsvReader]:
         n_blocks = BlockList._get_n_blocks(lines_reader)
         for _ in range(n_blocks):
-            yield list(lines_reader.pop_until(END_LINE_REGEX))
+            yield ListCsvReader(list(lines_reader.pop_until(END_LINE_REGEX)))
             lines_reader.pop()  # drop end line
             lines_reader.drop_empty()
 
@@ -882,5 +882,5 @@ class Data:
         return plate_blocks[0]
 
     @staticmethod
-    def create(lines_reader: LinesReader) -> Data:
+    def create(lines_reader: CsvReader) -> Data:
         return Data(block_list=BlockList.create(lines_reader))
